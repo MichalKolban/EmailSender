34089447bd4f9286a0668235710e1139e66713b3
Michal Kolban <michal.kolban@gmail.com>
2018-02-17 15:05:55 +0100

    App 1.1 nieskonczony serwer

---
+++ src/main/java/pl/sda/App.java
@@ -0,0 +1,13 @@
+package pl.sda;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        EmailService emailService = new EmailService();
+        EmailUserInfo emailUserInfo = new EmailUserInfo();
+        emailService.EmailService();
+    }
+}
